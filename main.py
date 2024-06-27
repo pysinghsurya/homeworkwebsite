d1c7d31799c6c598c5b340f4fb9948bb5ba62653
suryapart <suryapartap.xyz@gmail.com>
2024-06-27 17:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ password = ""
 name = ""
 
 app = Flask(__name__)
-my_email = "sirsuryapartap@gmail.com"
-my_password = "ybxr hlrt tlvw yuvg"
+my_email = "suryapartap.xyz@gmail.com"
+my_password = "bqco thiu mvwt isbi"
 year = datetime.now().year
 os.environ["FLASK_KEY"] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
